b81c77aff91080ab3872b6c2518ef6e19f33128b
Arijit Roy <royarijit1995@gmail.com>
2022-05-13 13:22:48 +0530

    Restructure the codebase
    *	Created seperate files for each class
    *	Added gitignore
    *	Untrack input and output files
    *	Updated main code to use tkinter instead of cli

--- diff-pdf.py
+++ diff-pdf.py
@@ -1,302 +1,46 @@
-import sys
-import os
-import argparse
 from io import StringIO
-from pdfminer.converter import TextConverter, PDFConverter, PDFPageAggregator
-from pdfminer.layout import LTTextBox, LAParams
-from pdfminer.layout import LTTextLine
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PdfToTextConverter import PdfToText
 
 
-class PdfToText(object):
-    ROBOT_LIBRARY_SCOPE = 'Global'
-
-    def __init__(self):
-        self.codec = 'utf-8'
-        self.scale = 1
-
-    def convert_pdf_to_txt(self, path, page_no=-1):
-        rsrcmgr = PDFResourceManager()
-
-        retstr = StringIO()
-        laparams = LAParams()
-        device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-        fp = open(path, 'rb')
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        password = ""
-        maxpages = 0
-        caching = True
-        pagenos = set()
-        i = 0
-        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
-                                      check_extractable=True):
-            i += 1
-            if i != page_no:
-                continue
-            interpreter.process_page(page)
-
-        fp.close()
-        device.close()
-        str = retstr.getvalue()
-        retstr.close()
-
-        return str
-
-    def read_pdf(self, file_name):
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams()
-        fp = open(file_name, 'rb')
-        parser = PDFParser(fp)
-        document = PDFDocument(parser)
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-        pages = {}
-        for page in PDFPage.create_pages(document):
-            dictionary = {}
-            dictionary['textbox'] = []
-            dictionary['textline'] = []
-
-            interpreter.process_page(page)
-            layout = device.get_result()
-            for item in layout:
-                if isinstance(item, LTTextBox):
-                    dictionary['textbox'].append(item)
-                    for child in item:
-                        if isinstance(child, LTTextLine):
-                            dictionary['textline'].append(child)
-            pages[layout.pageid] = dictionary
-        return pages
-
-    def compare_pdf(self, file1, file2, header_text, x_margin=10, compare_margin=0.2):
-        rsrcmgr = PDFResourceManager()
-        retstr = StringIO()
-        laparams = LAParams()
-        fp = open(file1, 'rb')
-        parser = PDFParser(fp)
-        document = PDFDocument(parser)
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-
-        out = StringIO()
-
-        layoutmode = 'normal'
-        scale = 1.3
-        fontscale = 1
-
-        html_coverter = HTMLPrivateConverter(rsrcmgr, out, scale=scale,
-                                             layoutmode=layoutmode, laparams=laparams, fontscale=fontscale,
-                                             imagewriter=None, header_text=header_text, x_margin=x_margin)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        testpages = PDFPage.create_pages(document)
-
-        file_dict = self.read_pdf(file2)
-
-        for page in testpages:
-            interpreter.process_page(page)
-            layout = device.get_result()
-
-            html_coverter.page_begin(layout)
-            if file_dict.get(layout.pageid) == None:
-                break
-            compare_page = file_dict[layout.pageid]
-
-            for item in layout:
-                if isinstance(item, LTTextBox):
-                    html_coverter.begin_div('textbox', 1, item.x0 + html_coverter.x_margin, item.y1, item.width,
-                                            item.height,
-                                            item.get_writing_mode())
-
-                    for child in item:
-                        if isinstance(child, LTTextLine):
-                            self.compare_textline(child, compare_page, html_coverter, compare_margin)
-                            html_coverter.put_newline()
-                    html_coverter.end_div()
-            html_coverter.page_end()
-
-        fp.close()
-        device.close()
-        retstr.close()
-
-        return out.getvalue()
-
-    def compare_textline(self, child, compare_page, html_coverter, compare_margin):
-        comp_result = True
-        for comp_textline in compare_page['textline']:
-            if child.x0 - compare_margin < comp_textline.x0 and comp_textline.x0 < child.x0 + compare_margin and child.y1 - compare_margin < comp_textline.y1 and comp_textline.y1 < child.y1 + compare_margin:
-                if child.get_text() != comp_textline.get_text():
-                    html_coverter.put_text_invalid(child.get_text(), child._objs[0].fontname, child._objs[0].size)
-                else:
-                    html_coverter.put_text(child.get_text(), child._objs[0].fontname, child._objs[0].size)
-
-                comp_result = False
-                break
-
-        if comp_result:
-            html_coverter.put_text_invalid(child.get_text(), child._objs[0].fontname, child._objs[0].size)
-
-    def convert_list(self, obj):
-        list = []
-        for child in obj:
-            list.append(child)
-        return list
+global source_file_path, target_file_path, margin_value, output_file_path
+source_file_path = ''
+target_file_path = ''
+margin_value = 0.2
+output_file_path = 'output.html'
 
 
-##  HTMLConverter
-class HTMLPrivateConverter(PDFConverter):
-    RECT_COLORS = {
-        'figure': 'yellow',
-        'textline': 'magenta',
-        'textbox': 'cyan',
-        'textgroup': 'red',
-        'curve': 'black',
-        'page': 'gray',
-    }
+def get_source_file_path():
+    global source_file_path
+    source_file_path = filedialog.askopenfilename(filetypes=[("PDF files", ".pdf"), ('All Files', '*.*')])
+    messagebox.showinfo("INFO", f"Source file {source_file_path} uploaded.")
+    print(source_file_path)
 
-    TEXT_COLORS = {
-        'textbox': 'blue',
-        'char': 'black',
-    }
 
-    def __init__(self, rsrcmgr, outfp, pageno=1, laparams=None,
-                 scale=1, fontscale=1.0, layoutmode='normal', showpageno=True,
-                 pagemargin=50, imagewriter=None, header_text='', x_margin=0,
-                 rect_colors={'curve': 'black', 'page': 'gray'},
-                 text_colors={'char': 'black'}):
-        PDFConverter.__init__(self, rsrcmgr, outfp, pageno=pageno, laparams=laparams)
-        self.scale = scale
-        self.fontscale = fontscale
-        self.layoutmode = layoutmode
-        self.showpageno = showpageno
-        self.pagemargin = pagemargin
-        self.imagewriter = imagewriter
-        self.rect_colors = rect_colors
-        self.text_colors = text_colors
-        self.debug = False
-        if self.debug:
-            self.rect_colors.update(self.RECT_COLORS)
-            self.text_colors.update(self.TEXT_COLORS)
-        self._yoffset = self.pagemargin
-        self._font = None
-        self._ffont = ('AllAndNone', 11)
-        self._fontstack = []
-        self.header_text = header_text
-        self.x_margin = x_margin
-        self.write_header()
-        return
+def get_target_file_path():
+    global target_file_path
+    target_file_path = filedialog.askopenfilename(filetypes=[("PDF files", ".pdf"), ('All Files', '*.*')])
+    messagebox.showinfo("INFO", f"Target file {target_file_path} uploaded.")
+    print(target_file_path)
 
-    def write(self, text):
-        self.outfp.write(text)
-        return
 
-    def write_header(self):
-        if self.x_margin != 10:
-            self.write('<div><span style="position:absolute; color:%s; left:%dpx; top:%dpx; font-size:%dpx;">' %
-                       (1, self.x_margin + 200, 0, 30 * self.scale * 1))
-        else:
-            self.write('<div><span style="position:absolute; color:%s; left:%dpx; top:%dpx; font-size:%dpx;">' %
-                       (1, self.x_margin + 7, 0, 30 * self.scale * 1))
-        self.write_text(self.header_text)
-        self.write('</span></div>\n')
-        return
+def update_output_filename(entry):
+    global output_file_path
+    output_file_path = entry.get()
+    if output_file_path is None or output_file_path == '':
+        output_file_path = 'output.html'
+    print(output_file_path)
 
-    def write_footer(self):
-        self.write('</body></html>\n')
-        return
 
-    def write_text(self, text):
-        self.write(text)
-        return
-
-    def place_rect(self, color, borderwidth, x, y, w, h):
-        color = self.rect_colors.get(color)
-        if color is not None:
-            self.write('<span style="position:absolute; border: %s %dpx solid; '
-                       'left:%dpx; top:%dpx; width:%dpx; height:%dpx;"></span>\n' %
-                       (color, borderwidth,
-                        x * self.scale, (self._yoffset - y) * self.scale,
-                        w * self.scale, h * self.scale))
-        return
-
-    def place_border(self, color, borderwidth, item):
-        self.place_rect(color, borderwidth, item.x0 + self.x_margin, item.y1, item.width, item.height)
-        return
-
-    def place_image(self, item, borderwidth, x, y, w, h):
-        if self.imagewriter is not None:
-            name = self.imagewriter.export_image(item)
-            self.write('<img src="%s" border="%d" style="position:absolute; left:%dpx; top:%dpx;" '
-                       'width="%d" height="%d" />\n' %
-                       (name, borderwidth,
-                        x * self.scale, (self._yoffset - y) * self.scale,
-                        w * self.scale, h * self.scale))
-        return
-
-    def place_text(self, color, text, x, y, size):
-        color = self.text_colors.get(color)
-        if color is not None:
-            self.write('<span style="position:absolute; color:%s; left:%dpx; top:%dpx; font-size:%dpx;">' %
-                       (color, x * self.scale, (self._yoffset - y) * self.scale, size * self.scale * self.fontscale))
-            self.write_text(text)
-            self.write('</span>\n')
-        return
-
-    def begin_div(self, color, borderwidth, x, y, w, h, writing_mode=False):
-        self._fontstack.append(self._font)
-        self._font = None
-        self.write('<div style="position:absolute; border: %s %dpx solid; writing-mode:%s; '
-                   'left:%dpx; top:%dpx; width:%dpx; height:%dpx;">' %
-                   (color, borderwidth, writing_mode,
-                    x * self.scale, (self._yoffset - y) * self.scale,
-                    w * self.scale, h * self.scale))
-        return
-
-    def end_div(self):
-        if self._font is not None:
-            self.write('</span>')
-        self._font = self._fontstack.pop()
-        self.write('</div>')
-        return
-
-    def put_text(self, text, fontname, size):
-        self.write('<span style="font-family: %s; font-size:%dpx; letter-spacing:-1.2;">' %
-                   (fontname, size * self.scale * self.fontscale))
-        self.write_text(text)
-        self.write('</span>')
-
-        return
-
-    def put_text_invalid(self, text, fontname, size):
-        self.write(
-            '<span style="font-family: %s; font-size:%dpx; background-color:rgb(255,153,153); letter-spacing:-1.2;">' %
-            (fontname, size * self.scale * self.fontscale))
-        self.write_text(text)
-        self.write('</span>')
-        return
-
-    def page_begin(self, layout):
-        self._yoffset += layout.y1
-        self.place_border('page', 1, layout)
-        if self.x_margin != 10:
-            self.write('<div style="position:absolute; left:%dpx; top:%dpx;">' % (
-                self.x_margin + 200, (self._yoffset - layout.y1) * self.scale))
-        else:
-            self.write('<div style="position:absolute; left:%dpx; top:%dpx;">' % (
-                self.x_margin + 7, (self._yoffset - layout.y1) * self.scale))
-        self.write('<a name="%s">Page %s</a></div>\n' % (layout.pageid, layout.pageid))
-
-    def page_end(self):
-        self._yoffset += self.pagemargin
-
-    def put_newline(self):
-        self.write('<br>\n')
-        return
-
-    def close(self):
-        self.write_footer()
-        return
+def update_margin(entry):
+    global margin_value
+    margin_value = entry.get()
+    try:
+        margin_value = float(margin_value)
+    except:
+        margin_value = 0.2
+    print(margin_value)
 
 
 def merge_html(output_html1, output_html2):
@@ -318,15 +62,26 @@ def merge_html(output_html1, output_html2):
 
 
 def compare_pdf(file1, file2, output_file, compare_margin=0.2):
-    pdf_reader = PdfToText()
+    if file1 is not None and file1 != '' and file2 is not None and file2 != '':
+        pdf_reader = PdfToText()
 
-    output_html1 = pdf_reader.compare_pdf(file1, file2, 'AS-IS', compare_margin=compare_margin)
-    output_html2 = pdf_reader.compare_pdf(file2, file1, 'TO-BE', 650, compare_margin=compare_margin)
+        output_html1 = pdf_reader.compare_pdf(file1, file2, 'AS-IS', compare_margin=compare_margin)
+        output_html2 = pdf_reader.compare_pdf(file2, file1, 'TO-BE', 650, compare_margin=compare_margin)
 
-    html = merge_html(output_html1, output_html2)
+        html = merge_html(output_html1, output_html2)
 
-    f_out = open(output_file, 'w', -1, 'utf-8')
-    f_out.write(html)
+        f_out = open(output_file, 'w', -1, 'utf-8')
+        f_out.write(html)
+        messagebox.showinfo("INFO", f"Comparison completed. \nResult saved in {output_file} file.")
+
+        global source_file_path, target_file_path, margin_value, output_file_path
+        source_file_path = ''
+        target_file_path = ''
+        margin_value = 0.2
+        output_file_path = 'output.html'
+
+    else:
+        messagebox.showerror("ERROR", "Please Upload a file first")
 
 
 def replace_string_to_sql_format(sql_str):
@@ -337,26 +92,49 @@ def replace_string_to_sql_format(sql_str):
 
 
 def main():
-    description = ('Compare PDF documents using PDF Miner '
-                   'and print out the differences as HTML documents')
-    parser = argparse.ArgumentParser(description=description)
+    global source_file_path, target_file_path, margin_value, output_file_path
+    scores = tk.Tk()
+    scores.geometry('350x200')
+    scores.winfo_screenwidth(), scores.winfo_screenheight()
+    scores.resizable(width=False, height=False)
 
-    parser.add_argument('files', nargs='*', help='compare files')
-    parser.add_argument('-m', '--compare-margin', default=0.2, type=float, help='object comparison margin')
-    parser.add_argument('-o', '--output-file', default='output.html', type=str, help='output file name')
+    label1 = tk.Label(scores, text="PDF Diff Tool", font=("Arial", 20))
+    label1.grid(row=0, column=0, columnspan=4)
 
-    args = parser.parse_args()
+    uploadFile1 = tk.Button(
+        scores,text="Upload Source File",width=20,command=get_source_file_path)
+    uploadFile1.grid(row=1, column=0, pady=7)
 
-    def error_message(msg):
-        sys.stderr.write('ERROR: %s%s' % (msg, os.linesep))
-        parser.print_usage(sys.stderr)
-        sys.exit(1)
+    uploadFile2 = tk.Button(
+        scores,text="Upload Target File",width=20,command=get_target_file_path)
+    uploadFile2.grid(row=1, column=1, padx=10, pady=7)
 
-    if len(args.files) != 2:
-        error_message('please input two pdf files, ex) python diff-pdf.py <file1> <file2>')
-    else:
-        compare_pdf(args.files[0], args.files[1], args.output_file, args.compare_margin)
+    output_file = tk.Entry(scores)
+    output_file.insert(0, output_file_path)
+    output_file.grid(row=2, column=0, pady=5)
+    update_output_file_btn = tk.Button(
+        scores, text="Update Output File Name", width=20, command=lambda: update_output_filename(output_file))
+    update_output_file_btn.grid(row=2, column=1, padx=10, pady=5)
+    
+    margin = tk.Entry(scores)
+    margin.insert(0, margin_value)
+    margin.grid(row=3, column=0, pady=5)
+    update_margin_btn = tk.Button(
+        scores, text="Update Margin Value", width=20, command=lambda: update_margin(margin))
+    update_margin_btn.grid(row=3, column=1, padx=10, pady=5)
+
+    dfButton = tk.Button(
+        scores,text="PDF Diff",width=20,bg="green",fg="white",
+        command=lambda: compare_pdf(source_file_path,target_file_path, output_file_path, margin_value))
+    dfButton.grid(row=4, column=0, pady=5)
+    
+    closeButton = tk.Button(
+        scores,text="Close",width=20,bg="red",fg="white",command=exit)
+    closeButton.grid(row=4, column=1, padx=10, pady=5)
+
+    scores.mainloop()
 
 
 if __name__ == "__main__":
     main()
+
